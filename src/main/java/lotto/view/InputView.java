7f17af703c48d74e85a0ad08745ab2bb52a8ce1a
sun <sun007021@gmail.com>
2025-11-03 00:00:24 +0900

    refactor(view): rename buy amount methods and constants for consistency

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,12 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_BUY_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_Purchas_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String readBuyAmount() {
-        System.out.println(INPUT_BUY_AMOUNT);
+    public static String readPurchasAmount() {
+        System.out.println(INPUT_Purchas_AMOUNT);
         return Console.readLine();
     }
 
