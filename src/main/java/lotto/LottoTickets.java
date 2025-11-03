0bf5cf5b9164e36d7c355c0844815259cf2510be
sun <sun007021@gmail.com>
2025-11-03 14:19:59 +0900

    fix(lottoTickets): change getAllLottoNumbers response to immutability List

--- src/main/java/lotto/LottoTickets.java
+++ src/main/java/lotto/LottoTickets.java
@@ -1,9 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LottoTickets {
     private final List<Lotto> lottos;
@@ -16,13 +14,9 @@ public class LottoTickets {
         return lottos.size();
     }
 
-    public List<Lotto> getLottos() {
-        return Collections.unmodifiableList(lottos);
-    }
-
     public List<List<Integer>> getAllLottoNumbers() {
         return lottos.stream()
                 .map(Lotto::getNumbers)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
